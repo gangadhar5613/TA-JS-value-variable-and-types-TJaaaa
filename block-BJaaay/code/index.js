93a13d75e3e39b4a11bff1ca4cd6a8bcf9bd7e4c
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2020-08-15 09:43:48 +0530

    Complete assignment on loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,31 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for (i =1; i<=10; i = i+1){
+    console.log(i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
-
+for(i=1;i<=10;i=i+1){
+ if(i % 2 == 0){
+     console.log(i);
+ }
+}
 // Using `console.log` log all the od values from 1 to 10.
-
+for(i=1;i<=10;i=i+1){
+    if(i % 2 != 0){
+        console.log(i);
+    }
+   }
 // Calculate the sum of all numbers from 1 to 10.
+for(i=10;i<=10;i=i+1){
+    let sum = i*(i+1)/2;
+    console.log(sum);
+}
 
 // Log all the values from 1 to 10 using while loop
+let x =0;
+while(x <10){
+    x = x+1;
+    console.log(x);
+}
